6fdef7e52870c08c59565eb8897a18cdcddf5957
Unmoy <vampireklug@gmail.com>
2024-09-05 14:19:50 +0600

    updates

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,12 +2,13 @@ import TextField from "@mui/material/TextField";
 import "./SignUp.css";
 import { useNavigate } from "react-router-dom";
 import Wrapper from "../Wrapper/Wrapper";
-
+import logo from "../../assets/images/ge3s_logo.png";
 export default function SignUp() {
   const navigate = useNavigate();
   return (
     <Wrapper>
       <div className="signup_page">
+        <img src={logo} alt="logo" className="brand_logo" />
         <h1>Create a free account!</h1>
         <div className="email_field">
           <p>Email</p>
