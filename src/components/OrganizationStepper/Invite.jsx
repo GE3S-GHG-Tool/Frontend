e4e1ba49d85c7f6f8f32aab9139c599eb396f441
Unmoy <vampireklug@gmail.com>
2024-09-10 13:02:39 +0600

    signup pages flow

--- src/components/OrganizationStepper/Invite.jsx
+++ src/components/OrganizationStepper/Invite.jsx
@@ -2,58 +2,150 @@ import { TextField } from "@mui/material";
 import logo from "../../assets/images/ge3s_logo.png";
 import PricingModal from "./PricingModal";
 import { useState } from "react";
+import SuccessAnimation from "../common/SuccessAnimation";
+import { useNavigate } from "react-router-dom";
 const Invite = () => {
   const [open, setOpen] = useState(false);
+  const [screen, setScreen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const navigate = useNavigate();
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    navigate("/home");
+  };
   return (
     <div className="invite_Page">
       <div className="heading">
         <img src={logo} alt="" className="ge3s_logo1" />
         <h1>Invite your team members and collaborate</h1>
       </div>
-      <h3>
-        You can invite upto “n” team members and work together to generate
-        Reports.
-      </h3>
 
-      <div>
-        <TextField
-          variant="outlined"
-          size="small"
-          fullWidth
-          margin="normal"
-          placeholder="Enter Email Address"
-        />
-        <TextField
-          variant="outlined"
-          size="small"
-          fullWidth
-          margin="normal"
-          placeholder="Enter Email Address"
-        />
-        <TextField
-          variant="outlined"
-          size="small"
-          fullWidth
-          margin="normal"
-          placeholder="Enter Email Address"
-        />
-        <TextField
-          variant="outlined"
-          size="small"
-          fullWidth
-          margin="normal"
-          placeholder="Enter Email Address"
-        />
-      </div>
+      <>
+        {screen ? (
+          <>
+            <h3>Invitations sent to</h3>
 
-      <div className="flex_box_invite">
-        <button className="skip" onClick={handleOpen}>
-          Skip
-        </button>
-        <button className="accept">Invite All</button>
-      </div>
+            <div className="email_list">
+              <div className="email_item">
+                <p>unmoy@growht.in</p>
+                <svg width="20" height="20" viewBox="0 0 24 25" fill="none">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M1.5 12.5003C1.50036 7.49102 5.03917 3.17959 9.95224 2.20266C14.8653 1.22573 19.7843 3.85539 21.701 8.48346C23.6177 13.1115 21.9982 18.449 17.833 21.2318C13.6678 24.0146 8.1169 23.4675 4.575 19.9253C2.60591 17.956 1.4998 15.2851 1.5 12.5003Z"
+                    stroke="#3CB477"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M6.75 12.4995L10.2495 15.999L17.25 9"
+                    stroke="#3CB477"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="email_item">
+                <p>unmoy@growht.in</p>
+                <svg width="20" height="20" viewBox="0 0 24 25" fill="none">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M1.5 12.5003C1.50036 7.49102 5.03917 3.17959 9.95224 2.20266C14.8653 1.22573 19.7843 3.85539 21.701 8.48346C23.6177 13.1115 21.9982 18.449 17.833 21.2318C13.6678 24.0146 8.1169 23.4675 4.575 19.9253C2.60591 17.956 1.4998 15.2851 1.5 12.5003Z"
+                    stroke="#3CB477"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M6.75 12.4995L10.2495 15.999L17.25 9"
+                    stroke="#3CB477"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="email_item">
+                <p>unmoy@growht.in</p>
+                <svg width="20" height="20" viewBox="0 0 24 25" fill="none">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M1.5 12.5003C1.50036 7.49102 5.03917 3.17959 9.95224 2.20266C14.8653 1.22573 19.7843 3.85539 21.701 8.48346C23.6177 13.1115 21.9982 18.449 17.833 21.2318C13.6678 24.0146 8.1169 23.4675 4.575 19.9253C2.60591 17.956 1.4998 15.2851 1.5 12.5003Z"
+                    stroke="#3CB477"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M6.75 12.4995L10.2495 15.999L17.25 9"
+                    stroke="#3CB477"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="finish_invte">
+              <button className="accept" onClick={() => setOpenModal(true)}>
+                Finish
+              </button>
+            </div>
+            <SuccessAnimation open={openModal} handleClose={handleCloseModal} />
+          </>
+        ) : (
+          <>
+            <h3>
+              You can invite upto “n” team members and work together to generate
+              Reports.
+            </h3>
+            <div>
+              <TextField
+                variant="outlined"
+                size="small"
+                fullWidth
+                margin="normal"
+                placeholder="Enter Email Address"
+              />
+              <TextField
+                variant="outlined"
+                size="small"
+                fullWidth
+                margin="normal"
+                placeholder="Enter Email Address"
+              />
+              <TextField
+                variant="outlined"
+                size="small"
+                fullWidth
+                margin="normal"
+                placeholder="Enter Email Address"
+              />
+              <TextField
+                variant="outlined"
+                size="small"
+                fullWidth
+                margin="normal"
+                placeholder="Enter Email Address"
+              />
+            </div>
+            <div className="flex_box_invite">
+              <button className="skip" onClick={handleOpen}>
+                Skip
+              </button>
+              <button className="accept" onClick={() => setScreen(true)}>
+                Invite All
+              </button>
+            </div>
+          </>
+        )}
+      </>
 
       <PricingModal open={open} handleClose={handleClose} />
     </div>
