8637a5585589abb3ffec73ebfa685cda39d9bdaa
abhijay-srivastava-gh <abhijay@growhut.in>
2024-11-19 19:18:57 +0530

    fixed login issues

--- src/components/OrganizationStepper/Goals.jsx
+++ src/components/OrganizationStepper/Goals.jsx
@@ -109,7 +109,7 @@ export default function Goals({ setActiveStep }) {
       );
       console.log("response", response);
       if (response.status === 201) {
-        navigate("/");
+        navigate("/login");
         localStorage.removeItem("token");
         // console.log("response", response);
       } else {
